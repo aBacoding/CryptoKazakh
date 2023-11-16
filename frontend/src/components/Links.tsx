be604c9506e0ae1409a7c8d17d6b5fd71a6db045
Abdurakhim B <97093590+aBacoding@users.noreply.github.com>
2023-11-17 04:41:16 +0600

    Update Links.tsx for vercel application

--- frontend/src/components/Links.tsx
+++ frontend/src/components/Links.tsx
@@ -4,10 +4,10 @@ const Links: React.FC = () => {
 	return (
 		<div className='link_imgs'>
 			<a href='https://chainlist.org/?search=11155111&testnets=true'>
-				<img src='./assets/image/Vector1.png' alt='vector' />
+				<img src='./assets/image/vector1.png' alt='vector' />
 			</a>
 			<a href='https://support.metamask.io/hc/en-us/articles/360015489531-Getting-started-with-MetaMask'>
-				<img src='./assets/image/Vector3.png' alt='vector' />
+				<img src='./assets/image/vector3.png' alt='vector' />
 			</a>
 		</div>
 	)
