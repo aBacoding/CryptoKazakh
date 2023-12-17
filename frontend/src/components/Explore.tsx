16f4767692659b76583c1a0762a6394372d5f633
Abdurakhim Bakytzhan <97093590+aBacoding@users.noreply.github.com>
2023-12-17 16:52:43 +0600

    Update Explore.tsx

--- frontend/src/components/Explore.tsx
+++ frontend/src/components/Explore.tsx
@@ -1,12 +1,12 @@
-import axios from 'axios';
-import { ethers } from 'ethers';
-import React, { useEffect, useState } from 'react';
-import '../styles/Explore.css';
-import Header from './Header';
+import axios from 'axios'
+import { ethers } from 'ethers'
+import React, { useEffect, useState } from 'react'
+import '../styles/Explore.css'
+import Header from './Header'
 
 const Explore: React.FC = () => {
-    const [nfts, setNfts] = useState<any[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [nfts, setNfts] = useState<any[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     const fetchNFTs = async () => {
         setIsLoading(true)
@@ -22,7 +22,7 @@ const Explore: React.FC = () => {
             const items = await contract.browseGallery()
             const itemsWithMetadata = await Promise.all(
                 items.map(async (item: any) => {
-                    const metadataUri = await contract.tokenURI(item.tokenId);
+                    const metadataUri = await contract.tokenURI(item.tokenId)
                     const metadataResponse = await axios.get(
                         `https://ipfs.io/ipfs/${metadataUri}`
                     )
@@ -40,89 +40,59 @@ const Explore: React.FC = () => {
             console.error('Error fetching NFTs: ', error)
         }
         setIsLoading(false)
-    };
-
-    const purchaseArtwork = async (tokenId: string, price: string) => {
-        if (!window.ethereum) {
-            alert('Please install MetaMask to make a purchase.')
-            return;
-        }
-
-        try {
-            const provider = new ethers.providers.Web3Provider(window.ethereum)
-            await provider.send('eth_requestAccounts', [])
-            const signer = provider.getSigner()
-            const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS || ''
-            const contractABI = require('../ArtCollectiveMarket.json').abi
-            const contract = new ethers.Contract(contractAddress, contractABI, signer)
-
-            const transaction = await contract.purchaseArtwork(
-                ethers.BigNumber.from(tokenId),
-                { value: ethers.utils.parseEther(price) }
-            );
-
-            await transaction.wait()
-            alert('Purchase successful!')
-
-            // Remove purchased NFT from the display
-            await fetchNFTs()
-        } catch (error) {
-            console.error('Purchase failed: ', error)
-            alert('There was an error processing your purchase.')
-        }
-    };
+    }
 
     useEffect(() => {
-        fetchNFTs();
+        fetchNFTs()
     }, [])
 
-    return (
-        <>
-            <Header />
-            <section className='explore_section'>
-                <div className='container'>
-                    <h2 className='explore_title'>Explore</h2>
-                    {isLoading ? (
-                        <p>Loading NFTs...</p>
-                    ) : nfts.length > 0 ? (
-                        <div className='nft-grid'>
-                            {nfts.map((nft, index) => (
-                                <div key={index} className='nft-card'>
-                                    <div className='image-container'>
-                                        <img
-                                            src={`https://ipfs.io/ipfs/${nft.image}`}
-                                            alt={nft.title}
-                                        />
-                                    </div>
-                                    <div className='nft-info'>
-                                        <h3>{nft.title}</h3>
-                                        <div className='artist'>
-                                            <img src={nft.avatar} alt='Artist Avatar' />
-                                            <span className='wallet-address'>
-                                                by @{nft.seller.substring(0, 6)}...
-                                            </span>
-                                        </div>
-                                        <div className='bid-info'>
-                                            <div className='current-bid'>Current Bid</div>
-                                            <p className='price'>{nft.price} ETH</p>
-                                        </div>
-                                        <button
-                                            className='place-bid'
-                                            onClick={() => purchaseArtwork(nft.tokenId, nft.price)}
-                                        >
-                                            Purchase
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    ) : (
-                        <p>No NFTs available.</p>
-                    )}
-                </div>
-            </section>
-        </>
-    );
-};
+	return (
+		<>
+			<Header userAddress={userAddress} setUserAddress={setUserAddress} />
+			<section className='explore_section'>
+				<div className='container'>
+					<h2 className='explore_title'>Explore</h2>
+					{isLoading ? (
+						<p>Loading NFTs...</p>
+					) : nfts.length > 0 ? (
+						<div className='nft-grid'>
+							{nfts.map((nft, index) => (
+								<div key={index} className='nft-card'>
+									<div className='image-container'>
+										<img
+											src={`https://ipfs.io/ipfs/${nft.image}`}
+											alt={nft.title}
+										/>
+									</div>
+									<div className='nft-info'>
+										<h3>{nft.title}</h3>
+										<div className='artist'>
+											<img src={nft.avatar} alt='Artist Avatar' />
+											<span className='wallet-address'>
+												by @{nft.seller.substring(0, 6)}...
+											</span>
+										</div>
+										<div className='bid-info'>
+											<div className='current-bid'>Current Bid</div>
+											<p className='price'>{nft.price} ETH</p>
+										</div>
+										<button
+											className='place-bid'
+											onClick={() => purchaseArtwork(nft.tokenId, nft.price)}
+										>
+											Purchase
+										</button>
+									</div>
+								</div>
+							))}
+						</div>
+					) : (
+						<p>No NFTs available.</p>
+					)}
+				</div>
+			</section>
+		</>
+	)
+}
 
-export default Explore;
+export default Explore
